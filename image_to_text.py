c78c2f53f87d1f5a9da4bcc5b12a136a260d9bee
hemantjoon <hemant.joon@rcb.res.in>
2024-06-30 12:10:24 +0200

    created main function and expanded help module
    Giving images as list
    Check for argparser initial
    appended description to describe the module
    expanding the help module and formatted it
    check for user argument, alongwith made --image argument as mandatory

--- image_to_text.py
+++ image_to_text.py
@@ -1,9 +1,8 @@
-# moving out the argparser, since it will be initiated once only
-# updating variables img_name_base from filename_original and img_name_gray from filename_gray, img_name from photo
-# making function img_to_gray with two arguments (img_name, img_name_gray)
-# appended with try except for img_to_gray so as to avoid in between function break
-# change psm default to 3 since giving good results for 3
-# append text.strip() /// translated.strip() to make sure empty lines are not printed
+# giving images as list
+# check for argparser initial
+# appended description to describe the module
+# expanding the help module and formatting it
+# check for user argument
 
 # Importing libraries
 import pytesseract
@@ -14,16 +13,6 @@ from deep_translator import GoogleTranslator
 pytesseract.tesseract_cmd = r"tesseract"
 
 
-img_list = ["im1.jpg", "hjk.mt", "img2.jpg"]
-
-# Fetching the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help="path to the input image", default="{name}")
-ap.add_argument("-l", "--lang", help="path to the language", default="eng")
-ap.add_argument("-t", "--to", type=str, help="eng")
-ap.add_argument("-p", "--psm", type=int, default=3, help="Tesseract PSM mode")
-args = vars(ap.parse_args())
-args["psm"] = "--psm "+str(args["psm"])
 
 def img_to_gray(img_name, img_name_gray):
     # Transform image from color to grayscale
@@ -32,11 +21,12 @@ def img_to_gray(img_name, img_name_gray):
             img.transform_colorspace("gray")
             img.adaptive_threshold(width=16, height=16, offset=-0.15 * img.quantum_range)
             img.save(filename=img_name_gray)
+            img.close()
             return True
     except:
             return False
     
-def google_translate(text, text_translated):
+def google_translate(text, text_translated, img_name, args):
     # Write-up translated text
     translated=GoogleTranslator(source="auto", target=args["to"]).translate(text)
     # translated = str(translated)
@@ -49,35 +39,58 @@ def google_translate(text, text_translated):
         print(f"No translation found {img_name}")
 
 
-for img_name in img_list:
-    # Declaring variables
-    img_name_base = img_name.rsplit(".",1)[0]
-    img_name_gray = "gray_" + img_name
-    text_en = "gray_"+img_name_base+".txt"
-    text_translated = "gray_translated_"+img_name_base+".txt"
+def my_iterator(args):
+    for img_name in args["image"]:
+        # break
+        # Declaring variables
+        img_name_base = img_name.rsplit(".",1)[0]
+        img_name_gray = "gray_" + img_name
+        text_en = "gray_"+img_name_base+".txt"
+        text_translated = "gray_translated_"+img_name_base+".txt"
 
-    check = img_to_gray(img_name, img_name_gray)
-    
+        check = img_to_gray(img_name, img_name_gray)
+        
 
-    if(check):
-        print(f"\nProcessing image {img_name}")
-        # Extract the text 
-        text = pytesseract.image_to_string(img_name_gray, lang=args["lang"], config=args["psm"])
-        # print(len(text.strip()))
+        if(check):
+            print(f"\nProcessing image {img_name}")
+            # Extract the text 
+            text = pytesseract.image_to_string(img_name_gray, lang=args["lang"], config=args["psm"])
 
-        if(text is not None and len(text.strip()) != 0):
-            # Write-up original text
-            with open(text_en, "w", encoding="utf-8") as outfile:
-                outfile.write(text)
-                outfile.close()
-            
-            print(f"Writing text {img_name}")
+            if(text is not None and len(text.strip()) != 0):
+                # Write-up original text
+                with open(text_en, "w", encoding="utf-8") as outfile:
+                    outfile.write(text)
+                    outfile.close()
+                
+                print(f"Writing text {img_name}")
 
-            if(args["to"] is not None):
-                google_translate(text, text_translated)
+                if(args["to"] is not None):
+                    google_translate(text, text_translated, img_name, args)
+
+            else:
+                print(f"No text captured {img_name}")
 
         else:
-            print(f"No text captured {img_name}")
+            print(f"\nNot a valid image {img_name} -- skipping")
 
-    else:
-         print(f"\nNot a valid image {img_name} -- skipping")
+
+
+def main():
+    # Fetching the arguments
+    ap = argparse.ArgumentParser(description="Python script to extract text from images after converting them to grayscale")
+    # Reference https://stackoverflow.com/questions/43786174/how-to-pass-and-parse-a-list-of-strings-from-command-line-with-argparse-argument
+    ap.add_argument("-i", "--image", help="path to the input image", required=True, nargs='+', default=[])
+    ap.add_argument("-l", "--lang", help="text language type (default: %(default)s)", default="eng", choices=pytesseract.get_languages(), metavar="eng")
+    ap.add_argument("-t", "--to", help="language to translate the text", choices=list((GoogleTranslator().get_supported_languages(as_dict=True)).values()), metavar="de")
+    ap.add_argument("-p", "--psm", type=int, default=3, help="Tesseract PSM mode type (default: %(default)s)", choices=range(1,14), metavar="3")
+    
+    args = vars(ap.parse_args())
+    args["psm"] = "--psm "+str(args["psm"])
+
+
+    my_iterator(args)
+
+
+
+if __name__ == "__main__":
+    main()
