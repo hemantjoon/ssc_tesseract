4fdfb59b68af53d77484e20fa92adce60e78554f
hemantjoon <hemant.joon@rcb.res.in>
2024-06-30 11:59:47 +0200

    string interpolation and variable name updated
    Correcting the comments
    from wand.image import Image and not Image as WImage since no Image variable clash is there
    updated .format() function with string interpolation wherever possible, ease of readability
    updated variable filenam to filename
    updated list with img_list and item with photo
    updated name to filename_gray
    creating all the variables at the start of loop for better usability
    removed paranthesis from GoogleTranslator

--- image_to_text.py
+++ image_to_text.py
@@ -1,53 +1,63 @@
-# single quotes to all double quotes && replacing psm 3 with psm only (noting down the range is from 0 to 14 -- detailed help)
-## observations : argparses is inside the image processing function itself (it should be separate and excecuted first)
-## observations : file open is handled differently also no file closing (more memory consumption)
-## hard-coded image file name
-## image extension is not checked; file is image or not??
-## output path not found, thus all files with same place as input and output
-## verbose may be added to have better progress report
-## try except for exceptions to be placed
-## extensive help module need to be generated
-## variable name should be more intuitive
-## functions should be made for processing and argparser
-## requirements.txt file if everything is good
+# correcting the comments
+# import wand.image import Image to only Image
+# removing the paranthesis from GoogleTranslator
+# String interpolation
+# updated rsplit to obtain the filename [0]
+# update filenam to filename
+# updated variable item to use filename
+# commenting out options variable as not being used in the script
+# changing list with img_list and item to photo
+# filenam to filename_original
+# name to filename_gray
 
 # Importing libraries
 import pytesseract
 import argparse
-from wand.image import Image as WImage
-from deep_translator import (GoogleTranslator)
+from wand.image import Image
+from deep_translator import GoogleTranslator
 
 pytesseract.tesseract_cmd = r"tesseract"
 
 
-list = ["im1.jpg", "img2.jpg"]
+img_list = ["im1.jpg", "img2.jpg"]
 
 
-for item in list:
-    print("Processing image {}".format(item))
-    filenam = item.rsplit(".",1)[1]
+for photo in img_list:
+    print(f"Processing image {photo}")
+
+    # Declaring variables
+    filename_original = photo.rsplit(".",1)[0]
+    filename_gray = "gray_" + photo
+    text_en = "gray_"+filename_original+".txt"
+    text_trans = "gray_translated_"+filename_original+".txt"
+
     # Transform image from color to grayscale
-    with WImage(filename=item) as img:
+    with Image(filename=photo) as img:
         img.transform_colorspace("gray")
         img.adaptive_threshold(width=16, height=16,
         offset=-0.15 * img.quantum_range)
-        img.save(filename="gray_"+item)   
-    # Extract the text 
+        img.save(filename=filename_gray)   
+
+    # Fetching the arguments
         ap = argparse.ArgumentParser()
         ap.add_argument("-i", "--image", help="path to the input image", default="{name}")
         ap.add_argument("-l", "--lang", help="path to the language", default="eng")
         ap.add_argument("-t", "--to", type=str, default="en", help="eng")
         ap.add_argument("-p", "--psm", type=int, default=13, help="Tesseract PSM mode")
         args = vars(ap.parse_args())
+
     # Set-up columns
-        options = "-l {} --psm {}".format(args["lang"], args["psm"])
-        name="gray_"+item
-        text = pytesseract.image_to_string(name)
+        # options = "-l {} --psm {}".format(args["lang"], args["psm"])
+
+    # Extract the text 
+        text = pytesseract.image_to_string(filename_gray)
+
     # Write-up original text
-        outfile = open("gray_"+item+".txt", "w", encoding="utf-8")
+        outfile = open(text_en, "w", encoding="utf-8")
         outfile.write(text)
+
     # Write-up translated text
         translated=GoogleTranslator(source="auto", target=args["to"]).translate(text)    
         trans=str(translated)
-        with open("gray_translated_"+item+".txt", "w", encoding="utf-8") as f:
+        with open(text_trans, "w", encoding="utf-8") as f:
             f.write(trans)
